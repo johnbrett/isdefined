ef3d02a5becafe52defd87760d25be9ee4f5e34e
John Brett <johnbrett7@gmail.com>
2014-11-24 22:10:55 +0000

    added more utility methods

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-module.exports = function(v) { return typeof v !== 'undefined' };
+var defined;
+
+/**
+ * Return whether variable is defined
+ * @param v
+ * @returns {boolean}
+ */
+defined.is_defined = function (v) {
+    return typeof v !== 'undefined';
+}
+
+/**
+ * If variable defined, return whether equals 1
+ * Else return undefined
+ * @param v
+ * @returns {boolean}
+ */
+defined.binary_to_boolean = function(v) {
+    return defined.is_defined(v) ? v == 1 : undefined;
+};
+
+/**
+ * If variable defined, return binary value of boolean
+ * Else return undefined
+ * @param v
+ * @returns {*}
+ */
+defined.boolean_to_binary = function(v) {
+    if(defined.is_defined(v)){
+        if(v == 'true' || v == true) {
+            return 1;
+        } else {
+            return 0;
+        }
+    } else {
+        return undefined;
+    }
+};
+
+module.exports = defined;
